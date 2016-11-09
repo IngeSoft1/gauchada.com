0d38e8b7f5d969587d9ded33d2a6199e3afa8c77
martinkelly23 <martkelly92@gmail.com>
2016-11-09 00:22:54 -0300

    form edit y new logro

--- app/controllers/logros_controller.rb
+++ app/controllers/logros_controller.rb
@@ -1,10 +1,40 @@
 class LogrosController < ApplicationController
+
+
   def index
   end
 
+  def show
+  end
+
   def new
+    @logro = Logro.new
+  end
+
+  def create
+    @logro = Logro.new(params[:valor_min])
+    if @logro.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   def edit
+    @logro = Logro.find(params[:id])
   end
+
+
+
+  def update
+   @logro = Logro.find params[:id]
+
+  if @logro.update_attributes params[:logro]
+    flash[:notice] = 'The Logro is successfully updated!'
+    redirect_to edit_user_path
+  end
+end
+
+    def destroy
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  
+
+  resources :logros
 
   get 'gauchada/index'
 
