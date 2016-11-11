341ed943ff41c58d4ee36b2ce356b509069c81f7
martinkelly23 <martkelly92@gmail.com>
2016-11-10 23:54:38 -0300

    validacion de logros

--- app/controllers/logros_controller.rb
+++ app/controllers/logros_controller.rb
@@ -12,12 +12,34 @@ class LogrosController < ApplicationController
 
   def create
     @logro = Logro.new(logro_params)
-    if @logro.save
-      flash[:notice] = 'El Logro se creó exitosamente!'
-      redirect_to listadoLogros_path
-    else
-      redirect_to listadoLogros_path
-    end
+    cumple = true
+
+    Logro.all.each do |logr|
+       if(@logro.nombre == logr.nombre)
+          flash[:notice] = 'Ya exite un logro con ese nombre'
+          cumple = false
+       end
+       if(@logro.valor_min <= logr.valor_max)
+         flash[:notice] = 'El puntaje del logro se superpone con otro'
+         cumple = false
+       end
+   end
+
+   if(@logro.valor_min >= @logro.valor_max)
+     flash[:notice] = 'El rango de punajes ingresado es inválido'
+     cumple = false
+   end
+
+   if(cumple)
+     if @logro.save
+       flash[:notice] = 'El Logro se creó exitosamente!'
+       redirect_to listadoLogros_path
+     else
+       redirect_to listadoLogros_path
+     end
+   else
+    redirect_to listadoLogros_path
+   end
   end
 
   def edit
@@ -30,10 +52,36 @@ class LogrosController < ApplicationController
 
   def update
    @logro = Logro.find params[:id]
-  if @logro.update_attributes (logro_params)
-    flash[:notice] = 'El Logro se actualizo exitosamente!'
-    redirect_to listadoLogros_path
+   nuevoLogro = Logro.new(logro_params)
+   cumple = true
+
+   Logro.all.each do |logr|
+      if(nuevoLogro.nombre == logr.nombre)&&(@logro.id != logr.id)
+         flash[:notice] = 'Ya exite un logro con ese nombre'
+         cumple = false
+      end
+      if(nuevoLogro.valor_min <= logr.valor_max)&&(nuevoLogro.valor_max >= logr.valor_min)&&(@logro.id != logr.id)
+        flash[:notice] = 'El puntaje del logro se superpone con otro'
+        cumple = false
+      end
   end
+
+  if(nuevoLogro.valor_min >= nuevoLogro.valor_max)
+    flash[:notice] = 'El rango de punajes ingresado es inválido'
+    cumple = false
+  end
+
+  if(cumple)
+    if @logro.update_attributes (logro_params)
+      flash[:notice] = 'El Logro se actualizo exitosamente!'
+      redirect_to listadoLogros_path
+    else
+      redirect_to listadoLogros_path
+    end
+  else
+   redirect_to listadoLogros_path
+  end
+
 end
 
     def destroy
