7acd2b3a249b246e5590c3ba60cc7d1a153326c5
martinkelly23 <martkelly92@gmail.com>
2016-12-01 11:39:25 -0300

    Se elimino la visualizacion de los comentarios para usuarios no registrados

--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20161128165539) do
     t.string   "ubicacion"
     t.string   "titulo"
     t.string   "imagen"
+    t.integer  "gauchada_id"
     t.integer  "postulacion_id"
     t.string   "estado"
   end
@@ -77,23 +78,6 @@ ActiveRecord::Schema.define(version: 20161128165539) do
     t.string   "estado"
   end
 
-  create_table "respuesta", force: :cascade do |t|
-    t.integer  "respuesta_id"
-    t.integer  "gauchada_id"
-    t.string   "comentario"
-    t.integer  "usuario_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
-
-  create_table "respuestas", force: :cascade do |t|
-    t.string   "res"
-    t.integer  "comentario_id"
-    t.integer  "usuario_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
@@ -111,6 +95,7 @@ ActiveRecord::Schema.define(version: 20161128165539) do
     t.datetime "date_of_birth"
     t.boolean  "admin",                  default: false
     t.string   "localidad"
+    t.integer  "gauchada_id"
     t.integer  "postulacion_id"
     t.integer  "puntos"
   end
