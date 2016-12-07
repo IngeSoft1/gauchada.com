1308a416ff65c70db87354cedc46174abf37e654
martinkelly23 <martkelly92@gmail.com>
2016-12-06 21:55:36 -0300

    pull

--- db/schema.rb
+++ db/schema.rb
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20161205151324) do
     t.string   "ubicacion"
     t.string   "titulo"
     t.string   "imagen"
+    t.integer  "gauchada_id"
     t.integer  "postulacion_id"
     t.string   "estado"
   end
@@ -80,15 +81,6 @@ ActiveRecord::Schema.define(version: 20161205151324) do
     t.string   "estado"
   end
 
-  create_table "respuesta", force: :cascade do |t|
-    t.text     "respuesta"
-    t.integer  "Id_usuario"
-    t.integer  "Id_gauchada"
-    t.integer  "Id_comentario"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
@@ -106,6 +98,7 @@ ActiveRecord::Schema.define(version: 20161205151324) do
     t.datetime "date_of_birth"
     t.boolean  "admin",                  default: false
     t.string   "localidad"
+    t.integer  "gauchada_id"
     t.integer  "postulacion_id"
     t.integer  "puntos"
   end
