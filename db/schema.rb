5d056a223e3fa7041669d06adc05623da5ac8cc0
martinkelly23 <martkelly92@gmail.com>
2016-12-06 22:33:25 -0300

    db

--- db/schema.rb
+++ db/schema.rb
@@ -105,26 +105,6 @@ ActiveRecord::Schema.define(version: 20161206022355) do
     t.string   "estado"
   end
 
-<<<<<<< HEAD
-=======
-  create_table "respuesta", force: :cascade do |t|
-    t.integer  "respuesta_id"
-    t.integer  "gauchada_id"
-    t.string   "comentario"
-    t.integer  "usuario_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
-
-  create_table "respuestas", force: :cascade do |t|
-    t.string   "res"
-    t.integer  "comentario_id"
-    t.integer  "usuario_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
->>>>>>> 969cf745a958d5ac689459443103c702c4c7dec0
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
